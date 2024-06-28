a54cc30df166d4c08ffc7b3144d2009212f630ae
Eddie610 <luwambo.edward@gmail.com>
2024-06-28 16:55:59 +0300

    chore:Implement ToDoList DB & ERD

---
+++ My_Todo_Project/add_task.php
@@ -0,0 +1,16 @@
+<?php
+require_once 'config.php';
+if (isset($_POST['add'])) {
+    if ( !empty($_POST['task'] /* != "" */)) {
+        $task = $_POST['task'];
+        /* $addtasks="INSERT INTO `task`(task,status) VALUES('', '$task', 'Pending') */
+ 
+        $addtasks = mysqli_query($db, 
+            "INSERT INTO  task(task,status) VALUES( '$task', 'Pending')");
+            header("refresh:2,url=My_Todo.php")
+            or
+            die(mysqli_error($db));
+        header('location:index.php');
+
+    }
+}
---
+++ My_Todo_Project/config.php
@@ -0,0 +1,5 @@
+<?php
+$db = mysqli_connect("localhost", "root", "", "my_todo")
+
+    or
+    die("Connection failed: " . mysqli_connect_error());
